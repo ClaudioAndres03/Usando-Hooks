9e242c9515e8da9e6a31513f1d068b00aa86c559
Claudio Andres <ClaudioAndres03@outlook.com>
2020-08-23 20:29:49 -0400

    Validacion si el json viene vacio

--- src/api/Jorge.js
+++ src/api/Jorge.js
@@ -1,5 +1,7 @@
 export function getVideos() {
   return fetch("https://goland-gin-poc.herokuapp.com/api/videos", {
     method: "GET",
-  }).then((reponse) => reponse.json());
+  })
+    .then((reponse) => reponse.json())
+    .then((json) => json || []);
 }
